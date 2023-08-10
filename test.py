dfbc4456a6b18dc885db2e4be997dc7ff51626c8
Steven Burnett <texruska@users.noreply.github.com>
2023-08-10 09:41:51 +0000

    Modify test to just use default options

--- test.py
+++ test.py
@@ -1,13 +1,4 @@
 import halftone
 
-h = halftone.Halftone("/Users/phil/Desktop/_testpics/cmyk.jpg")
-h.make(
-    style="color",
-    save_channels=True,
-    save_channels_style="color",
-    save_channels_format="png",
-    percentage=100,
-    antialias=True,
-    output_quality=80,
-    output_format="png",
-)
+h = halftone.Halftone("./examples/original.jpg")
+h.make(filename_addition="_test")
